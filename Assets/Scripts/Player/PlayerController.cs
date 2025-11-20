c18df8349bc6c1a99b1e6ef7773400d09c4910f0
Dohooyi <59619810+sisyphusman@users.noreply.github.com>
2025-11-20 22:56:40 +0900

    fix: 봇 리스폰 시간을 10초부터 30초까지 랜덤으로 변경

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1087,8 +1087,8 @@ public class PlayerController : NetworkBehaviour
     {
         //yield return botRespawnWait;  // GC 최적화: 캐싱된 WaitForSeconds 사용
 
-        // 7초에서 10초 사이의 랜덤 시간 설정
-        float randomRespawnTime = Random.Range(7f, 10f);
+        // 10초에서 30초 사이의 랜덤 시간 설정
+        float randomRespawnTime = Random.Range(10f, 30f);
         yield return new WaitForSeconds(randomRespawnTime);
         DoRespawnTeleport();
     }
